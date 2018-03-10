b8fa40591289c86c6c12d90eb99a84a524d99709
Cesar <33010096+kikhi@users.noreply.github.com>
2018-03-10 12:18:17 -0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Progr26_Silba_Tinoco_Daniel_Eduardo
+namespace Progr26_Trujillo_Garay_Cesar_Andres
 {
     public partial class Form1 : Form
     {
