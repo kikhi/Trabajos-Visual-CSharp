d83311bb22008c902e902aab6302ea0ed703a4cc
Cesar <33010096+kikhi@users.noreply.github.com>
2018-03-10 10:54:20 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_17_Paredes_Romero_Adan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int OP;
+            OP=int.Parse(textBox1.Text);
+            
+            if (OP==1)
+            {
+                Form DOS = new VOLTAJE ();
+                DOS.Show();
+                this.Hide();
+            }
+
+            if (OP==2)
+            {
+                Form TRES = new RESISTENCIA ();
+                TRES.Show();
+                this.Hide();
+            }
+            if (OP==3)
+            {
+                Form CUATRO = new CORRIENTE();
+                CUATRO.Show();
+                this.Hide();
+            }
+            if (OP==4)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
